3056d708ead9de2b6d22be12c989a8b60f56954d
Andry <andry.carolina88@gmail.com>
2023-08-01 23:11:13 -0500

    solution to the technical test

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 const App = () => {
   const [users, setUsers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState ('');
 
   useEffect(() => {
     fetchUsers();
@@ -9,7 +10,7 @@ const App = () => {
 
   const fetchUsers = async () => {
     try {
-      const response = await get('https://jsonplaceholder.typicode.com/users');
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
       if (!response.ok) {
         throw new Error('Failed to fetch users');
       }
@@ -19,6 +20,16 @@ const App = () => {
       console.error(error);
     }
   };
+    
+  const filterUser =  users.filter(
+    (user) => 
+    user.name.toLowerCase().includes(searchTerm.toLowerCase()) || 
+    user.email.toLowerCase().includes(searchTerm.toLowerCase())
+  )
+
+  const handleInputChange = (event) => {
+    setSearchTerm(event.target.value)
+  }
 
   return (
     <div>
@@ -26,9 +37,10 @@ const App = () => {
       <input
         type="text"
         placeholder="Buscar por nombre o correo electrónico"
+        onChange={handleInputChange}
       />
       <ul>
-        {users.map((user) => (
+        {filterUser.map((user) => (
           <li key={user.id}>
             <strong>{user.name}</strong>
             <p>{user.email}</p>
